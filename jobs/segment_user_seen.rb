1f2fd66529df10888a04402496bae0652bbafd67
Joe Buhlig <joe@joebuhlig.com>
2017-04-13 16:53:30 -0500

    Remove identify call from User Seen event

--- jobs/segment_user_seen.rb
+++ jobs/segment_user_seen.rb
@@ -5,21 +5,6 @@ module Jobs
     def execute(args)
       segment = DiscourseSegment::Common.connect
       user = User.find_by(id: args[:user_id])
-      user_field = UserField.find_by(name: "Organization")
-      org_type = user.user_fields[user_field.id.to_s]
-      segment.identify(
-        user_id: user.id,
-        traits: {
-          name: user.name,
-          username: user.username,
-          email: user.email,
-          org_type: org_type,
-          created_at: user.created_at
-        },
-        context: {
-          ip: user.ip_address
-        }
-      )
       segment.track(
         user_id: user.id,
         event: 'User Seen'
