2803b0e5a34cd157b471e96b3bba1e8138d1d82f
Joe Buhlig <joe@joebuhlig.com>
2017-04-14 09:24:50 -0500

    Add categories and tags to topic and post events

--- jobs/segment_after_create_post.rb
+++ jobs/segment_after_create_post.rb
@@ -5,13 +5,29 @@ module Jobs
     def execute(args)
       segment = DiscourseSegment::Common.connect
       post = Post.find_by(id: args[:post_id])
+      if post.topic.category.parent_category_id.nil?
+        category = post.topic.category.id
+        subcategory = nil
+      else
+        category = post.topic.category.parent_category_id
+        subcategory = post.topic.category.id
+      end
+      tags = ""
+      post.topic.topic_tags.each do |tag|
+        tags = "#{tags}#{tag.tag_id}|"
+      end
+      tags = tags[0...-1]
       segment.track(
         user_id: args[:user_id],
         event: 'Post Created',
         properties: {
           slug: post.topic.slug,
           title: post.topic.title,
-          url: post.topic.url
+          url: post.topic.url,
+          category: category,
+          subcategory: subcategory,
+          userTags: tags,
+          postedBy: args[:user_id]
         }
       )
       segment.flush
--- jobs/segment_after_edit_topic.rb
+++ jobs/segment_after_edit_topic.rb
@@ -5,13 +5,29 @@ module Jobs
     def execute(args)
       segment = DiscourseSegment::Common.connect
       topic = Post.find_by(id: args[:post_id]).topic
+      if topic.category.parent_category_id.nil?
+        category = topic.category.id
+        subcategory = nil
+      else
+        category = topic.category.parent_category_id
+        subcategory = topic.category.id
+      end
+      tags = ""
+      topic.topic_tags.each do |tag|
+        tags = "#{tags}#{tag.tag_id}|"
+      end
+      tags = tags[0...-1]
       segment.track(
         user_id: args[:user_id],
         event: 'Topic Edited',
         properties: {
           slug: topic.slug,
           title: topic.title,
-          url: topic.url
+          url: topic.url,
+          category: category,
+          subcategory: subcategory,
+          userTags: tags,
+          postedBy: args[:user_id]
         }
       )
       segment.flush
